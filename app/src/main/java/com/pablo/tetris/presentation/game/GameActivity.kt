e0cd87118315ea4dc0560243e66de312f877c818
Robert Batorowski <Robert.Batorowski@redwood.com>
2024-03-08 11:29:05 +0100

    feat: settings infra

--- app/src/main/java/com/pablo/tetris/presentation/game/GameActivity.kt
+++ app/src/main/java/com/pablo/tetris/presentation/game/GameActivity.kt
@@ -2,14 +2,15 @@ package com.pablo.tetris.presentation.game
 
 import android.os.Bundle
 import com.pablo.tetris.R
+import com.pablo.tetris.infra.settings.SettingsHelper
 import com.pablo.tetris.presentation.common.HideStatusBarActivity
-
 
 class GameActivity : HideStatusBarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-    }
 
+        SettingsHelper.save(this, SettingsHelper.load(this))
+    }
 }
