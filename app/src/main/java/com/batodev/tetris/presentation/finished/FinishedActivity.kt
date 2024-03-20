87387a82d81e41dc7efc8aca0d7107e67eb7316b
Bator <robbator@gmail.com>
2024-03-20 14:57:05 +0100

    feat: remove unused

--- app/src/main/java/com/batodev/tetris/presentation/finished/FinishedActivity.kt
+++ app/src/main/java/com/batodev/tetris/presentation/finished/FinishedActivity.kt
@@ -2,51 +2,25 @@ package com.batodev.tetris.presentation.finished
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.view.Gravity
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.batodev.tetris.R
 import com.batodev.tetris.databinding.ActivityFinishedBinding
-import com.batodev.tetris.infra.database.Player
-import com.batodev.tetris.infra.database.PlayerApplication
 import com.batodev.tetris.infra.images.ImageHelper
-import com.batodev.tetris.infra.logs.LoggerGetter
 import com.batodev.tetris.infra.settings.SettingsHelper
-import com.batodev.tetris.presentation.common.GAME_RESULT
 import com.batodev.tetris.presentation.common.HideStatusBarActivity
-import com.batodev.tetris.presentation.finished.sendmail.EmailData
-import com.batodev.tetris.presentation.finished.sendmail.EmailSender
 import com.batodev.tetris.presentation.gallery.GalleryActivity
 import com.batodev.tetris.presentation.gallery.IMAGES
 import com.batodev.tetris.presentation.game.GameActivity
 import com.batodev.tetris.presentation.game.fragments.IMAGES_WON_THIS_GAME
-import com.batodev.tetris.presentation.game.results.DateGetter
-import com.batodev.tetris.presentation.game.results.GameResult
-import com.batodev.tetris.presentation.settings.SettingsActivity
-import com.batodev.tetris.presentation.settings.SettingsSingleton
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.flow.collect
-import nl.dionsegijn.konfetti.core.Party
-import nl.dionsegijn.konfetti.core.Position
-import nl.dionsegijn.konfetti.core.emitter.Emitter
-import nl.dionsegijn.konfetti.xml.KonfettiView
-import java.util.concurrent.TimeUnit.MILLISECONDS
 
 
 class FinishedActivity : HideStatusBarActivity() {
 
     private lateinit var binding: ActivityFinishedBinding
-    private val model: FinishedViewModel by lazy {
-        ViewModelProvider(
-            this,
-            PlayerViewModelFactory((application as PlayerApplication).repository)
-        ).get(FinishedViewModel::class.java)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
